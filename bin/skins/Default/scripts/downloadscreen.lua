16f09477c20242aba76a6ae5b690bb18f3326e07
Drewol <emildraws@gmail.com>
2019-05-24 13:39:36 +0200

    Improve download screen visuals

--- bin/skins/Default/scripts/downloadscreen.lua
+++ bin/skins/Default/scripts/downloadscreen.lua
@@ -4,6 +4,12 @@ header["user-agent"] = "unnamed_sdvx_clone"
 
 local jacketFallback = gfx.CreateSkinImage("song_select/loading.png", 0)
 local diffColors = {{50,50,127}, {50,127,50}, {127,50,50}, {127, 50, 127}}
+local entryW = 770
+local entryH = 320
+local resX,resY = game.GetResolution()
+local xCount = math.floor(resX / entryW)
+local yCount = math.floor(resY / entryH)
+local xOffset = (resX - xCount * entryW) / 2
 local cursorPos = 0
 local cursorPosX = 0
 local cursorPosY = 0
@@ -25,6 +31,8 @@ function addsong(song)
     end
     table.insert(songs, song)
 end
+local yOffset = 0
+local backgroundImage = gfx.CreateSkinImage("bg.png", 1);
 
 dlcache = io.open(cachepath, "r")
 if dlcache then
@@ -36,8 +44,10 @@ function encodeURI(str)
         str = string.gsub(str, "\n", "\r\n")
         str = string.gsub(str, "([^%w ])",
             function (c) 
-                local dontChange = "-/_:"
-                if string.match(dontChange, c) then return c end
+                local dontChange = "-/_:."
+                for i = 1, #dontChange do
+                    if c == dontChange:sub(i,i) then return c end
+                end
                 return string.format ("%%%02X", string.byte(c)) 
             end)
         str = string.gsub(str, " ", "%%20")
@@ -64,14 +74,19 @@ Http.GetAsync(nextUrl, header, gotSongsCallback)
 
 
 function render_song(song, x,y)
-    gfx.Save()
+    gfx.Save()    
     gfx.TextAlign(gfx.TEXT_ALIGN_LEFT + gfx.TEXT_ALIGN_TOP)
-    gfx.FillColor(255,255,255)
     gfx.Translate(x,y)
-    gfx.FontSize(25)
-    gfx.Text(song.title, 0,0)
-    gfx.FontSize(20)
-    gfx.Text(song.artist, 0,30)
+    gfx.Scissor(0,0,750,300)
+    gfx.BeginPath()
+    gfx.FillColor(0,0,0,140)
+    gfx.Rect(0,0,750,300)
+    gfx.Fill()
+    gfx.FillColor(255,255,255)
+    gfx.FontSize(30)
+    gfx.Text(song.title, 2,2)
+    gfx.FontSize(24)
+    gfx.Text(song.artist, 2,26)
     if song.jacket_url ~= nil and song.jacket == jacketFallback then
         song.jacket = gfx.LoadWebImageJob(song.jacket_url, jacketFallback, 250, 250)
     end
@@ -79,7 +94,7 @@ function render_song(song, x,y)
     gfx.ImageRect(0, 50, 250, 250, song.jacket, 1, 0)
     gfx.BeginPath()
     gfx.Rect(250,50,500,250)
-    gfx.FillColor(55,55,55)
+    gfx.FillColor(55,55,55,128)
     gfx.Fill()
     for i, diff in ipairs(song.charts) do
         local col = diffColors[diff.difficulty]
@@ -104,33 +119,48 @@ function render_song(song, x,y)
         gfx.FillColor(255,255,255)
         gfx.Text(downloaded[song.id], 375, 150)
     end
+    gfx.ResetScissor()
     gfx.Restore()
 end
 
+function load_more()
+    if nextUrl ~= nil and not loading then
+        Http.GetAsync(nextUrl, header, gotSongsCallback)
+        loading = true
+    end
+end
+
 function render_cursor()
-    local x = displayCursorPosX * 800
-    local y = displayCursorPosY * 400
+    local x = displayCursorPosX * entryW
+    local y = displayCursorPosY * entryH
     gfx.BeginPath()
-    gfx.Rect(x-5,y-5,760,310)
-    gfx.FillColor(0,0,200)
-    gfx.Fill()
+    gfx.Rect(x,y,750,300)
+    gfx.StrokeColor(255,128,0)
+    gfx.StrokeWidth(5)
+    gfx.Stroke()
 end
 
 function render(deltaTime)
+    gfx.ImageRect(0, 0, resX, resY, backgroundImage, 1, 0);
     gfx.LoadSkinFont("NotoSans-Regular.ttf");
     displayCursorPosX = displayCursorPosX - (displayCursorPosX - cursorPosX) * deltaTime * 10
     displayCursorPosY = displayCursorPosY - (displayCursorPosY - cursorPosY) * deltaTime * 10
-    gfx.Translate(50, 50 - displayCursorPosY * 400)
-    render_cursor()
+    if displayCursorPosY - yOffset > yCount - 1 then --scrolling down
+        yOffset = yOffset - (yOffset - displayCursorPosY) - yCount + 1
+    elseif displayCursorPosY - yOffset < 0 then
+        yOffset = yOffset - (yOffset - displayCursorPosY)
+    end
+    gfx.Translate(xOffset, 50 - yOffset * entryH)
     for i, song in ipairs(songs) do
-        if math.abs(cursorPos - i) <= 10 then
+        if math.abs(cursorPos - i) <= xCount * yCount + xCount then
             i = i - 1
-            local x = 800 * (i % 2)
-            local y = (i - (i % 2)) * 200
+            local x = entryW * (i % xCount)
+            local y = math.floor(i / xCount) * entryH
             render_song(song, x, y)
-        
+            if math.abs(#songs - i) < 4 then load_more() end
         end
     end
+    render_cursor()
 end
 
 function archive_callback(entries, id)
@@ -165,11 +195,9 @@ end
 
 function advance_selection(steps)
     cursorPos = (cursorPos + steps) % #songs
-    cursorPosX = cursorPos % 2
-    cursorPosY = math.floor(cursorPos / 2)
-    if cursorPos > #songs - 6 and nextUrl ~= nil and not loading then
-        Http.GetAsync(nextUrl, header, gotSongsCallback)
-        loading = true
+    cursorPosX = cursorPos % xCount
+    cursorPosY = math.floor(cursorPos / xCount)
+    if cursorPos > #songs - 6 then
+        load_more()
     end
-    
 end
