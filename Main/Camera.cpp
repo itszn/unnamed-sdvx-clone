83a8f2c3fac13743a1bf01a09789ec48ed0310a3
ASleepyCat <dyeom340@gmail.com>
2019-11-27 23:28:14 +1000

    Remove slowest tilting

--- Main/Camera.cpp
+++ Main/Camera.cpp
@@ -133,8 +133,7 @@ void Camera::Tick(float deltaTime, class BeatmapPlayback& playback)
 	{
 		float speedLimitDivider = 1.f;
 		if (m_slowTilt && !rollCatchUp)
-			// Roll even slower when roll is less than 1 / 10 of max tilt
-			speedLimitDivider = fabsf(m_laserRoll) > m_rollIntensity * SLOWEST_TILT_THRESHOLD ? 4.f : 8.f;
+			speedLimitDivider = 4.4f;
 		LerpTo(m_laserRoll, m_targetLaserRoll, speedlimit / speedLimitDivider);
 	}
 	
