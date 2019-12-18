5ae24f4c02b376cdcf25c1c6d8902abd5dea1766
Drewol <emildraws@gmail.com>
2019-12-18 18:28:23 +0100

    Fix shaders for non-embedded build

--- bin/skins/Default/backgrounds/smoke/bg.fs
+++ bin/skins/Default/backgrounds/smoke/bg.fs
@@ -1,4 +1,3 @@
-#version 330
 #extension GL_ARB_separate_shader_objects : enable
 
 layout(location=1) in vec2 texVp;
--- bin/skins/Default/shaders/guiTex.fs
+++ bin/skins/Default/shaders/guiTex.fs
@@ -1,7 +1,7 @@
 #ifdef EMBEDDED
 varying vec2 fsTex;
 #else
-//#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_separate_shader_objects : enable
 layout(location=1) in vec2 fsTex;
 layout(location=0) out vec4 target;
 #endif
