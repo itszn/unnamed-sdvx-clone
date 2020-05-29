39c2a9bc4e542e60045ae9f4c97e21d65400c914
Jimin Park <pjm950@gmail.com>
2020-05-30 01:31:31 +0900

    Adjusted window size for calibration screen.
    The "Cancel" and "Ok" buttons should be visible by default.

--- Main/src/CalibrationScreen.cpp
+++ Main/src/CalibrationScreen.cpp
@@ -93,7 +93,10 @@ void CalibrationScreen::Render(float deltaTime)
 
 	//Draw nuklear GUI
 	{
-		if (nk_begin(m_ctx, "Options", nk_rect(50, 50, 400, 300), windowFlag))
+		const int OPTIONS_WINDOW_WIDTH = Math::Clamp(g_resolution.x - 50, 100, 400);
+		const int OPTIONS_WINDOW_HEIGHT = Math::Clamp(g_resolution.y - 50, 200, 520);
+
+		if (nk_begin(m_ctx, "Options", nk_rect(50, 50, OPTIONS_WINDOW_WIDTH, OPTIONS_WINDOW_HEIGHT), windowFlag))
 		{
 			nk_layout_row_dynamic(m_ctx, 30, 1);
 			m_audioOffset = nk_propertyi(m_ctx, "Global Offset", -1000, m_audioOffset, 1000, 1, 1);
