0c07ea770bf9b00552cac9331f33f9207f148a8a
ereti <43418802+ereti@users.noreply.github.com>
2020-12-08 00:03:22 +0000

    Remove test code that somehow slipped into a commit

--- bin/skins/Default/scripts/songselect/background.lua
+++ bin/skins/Default/scripts/songselect/background.lua
@@ -8,40 +8,10 @@ TEXT_ALIGN_MIDDLE	= 16;
 TEXT_ALIGN_BOTTOM	= 32;
 TEXT_ALIGN_BASELINE	= 64;
 local backgroundImage = gfx.CreateSkinImage("bg.png", 1);
-local foo = false;
-
-function resc(res)
-    game.Log(log_table(res), game.LOGGER_ERROR)
-
-end
-
-function log_table(table)
-    str = "{"
-    for k, v in pairs(table) do
-        str = str .. k .. ": "
-
-        t = type(v)
-
-        if t == "table" then
-            str = str .. log_table(v)
-        elseif t == "string" then
-            str = str .. "\"" .. v .. "\""
-        else
-            str = str .. v
-        end
-
-        str = str .. ", "
-    end
-
-    return str .. "}"
-end
 
 
 render = function(deltaTime)
-    if not foo then
-        foo = true
-        IR.Heartbeat(resc)
-    end
+
 
     resx,resy = game.GetResolution();
     gfx.BeginPath();
